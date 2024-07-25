5ff0624ac7eae540fa5a35e08f0fd167ecfddab9
Carlos Roberto <crrsj1@gmail.com>
2024-07-25 17:22:36 -0300

    implementando o metodo buscarPorId na classe PessoaControle

--- src/main/java/com/pessoas/controle/PessoaControle.java
+++ src/main/java/com/pessoas/controle/PessoaControle.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +37,12 @@ public class PessoaControle {
 		return ResponseEntity.ok(listar);	
 		
 	}
+	
+	@GetMapping
+	public ResponseEntity<PessoaDto>buscarPorId(@PathVariable Long id){
+		var buscaId = pessoaServico.buscarPorId(id);
+		return ResponseEntity.ok().body(new PessoaDto(buscaId));
+	}
+	
+	
 }
