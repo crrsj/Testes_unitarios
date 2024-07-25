2747bf2598ee0f2cd8908d90c19bc771074eeedc
Carlos Roberto <crrsj1@gmail.com>
2024-07-25 17:31:09 -0300

    implementando o metodo excluir na classe PessoaControle

--- src/main/java/com/pessoas/controle/PessoaControle.java
+++ src/main/java/com/pessoas/controle/PessoaControle.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,4 +50,10 @@ public class PessoaControle {
 		var atualize = pessoaServico.atualizarCadastro(pessoa, id);
 		return ResponseEntity.ok().body(new PessoaDto(atualize));
 	} 
+	
+	@DeleteMapping
+	public ResponseEntity<Void>excluir(){
+		pessoaServico.excluir(null);
+		return ResponseEntity.noContent().build();
+	}
 }
